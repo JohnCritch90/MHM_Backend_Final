bd52443546d2f275e2649514c18eebe7f681fbc3
JohnCritch90 <123122544+JohnCritch90@users.noreply.github.com>
2023-06-15 23:46:17 +0000

    test

--- src/api/models.py
+++ src/api/models.py
@@ -8,7 +8,7 @@ db = SQLAlchemy()
 class User(db.Model):
   __tablename__ = 'user'
   user_id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(255), unique=True, nullable=False)
+  username = db.Column(String(255), unique=True, nullable=False)
   email  = db.Column(String(255), unique=True, nullable=False)
   password = db.Column(String(255), unique=True, nullable=False)
 
--- src/api/routes.py
+++ src/api/routes.py
@@ -1,53 +1,58 @@
 import os  
 from flask import Flask ,request, jsonify, Blueprint 
-from api.models import db, User 
-from flask_jwt_extended import create_access_token, get_jwt_identify, jew_required 
-from flask_jwt_extended import JWTManager  
+from api.models import db, User
+# from flask_jwt_extended import create_access_token, get_jwt_identify, jew_required 
+# from flask_jwt_extended import JWTManager  
+# from models import db, User, JournalEntries, MentalHealthResources, MeditationSessions, SupportGroups, Notifications
+# from utils import APIException
 
 api = Blueprint('api', __name__)
 
+@api.route('/nhs_condition', methods=['GET'])
+def getCondition():
+    return jsonify('YOUR API WORKS, CONGRATS'), 200
 
-@api.route('/signup', methods=['POST'])
-def signup():
-    email = request.json.get('email')
-    first_name = request.json.get('first_name')
-    last_name = request.json.get('last_name')
-    password = request.json.get('password')
+# @api.route('/signup', methods=['POST'])
+# def signup():
+#     email = request.json.get('email')
+#     first_name = request.json.get('first_name')
+#     last_name = request.json.get('last_name')
+#     password = request.json.get('password')
 
-    if user.query.fliter_by(email=email).first():
-        return jsonify(message='Email already registered'), 200
+#     if User.query.fliter_by(email=email).first():
+#         return jsonify(message='Email already registered'), 200
 
-        new-user = User(email, first_name=first_name ,last_name=last_name,password=password, is_active=Ture)
+#         new-user == User(email, first_name=first_name ,last_name=last_name,password=password, is_active=Ture)
 
-        try:
-            db.session.add(new_user)
-            db.session.commit()
-        except Exception as e:
-            db.session.rollback()
-            return jsonify(message='failed to register user'), 200
+#         try:
+#             db.session.add(new_user)
+#             db.session.commit()
+#         except Exception as e:
+#             db.session.rollback()
+#             return jsonify(message='failed to register user'), 200
 
-        return jsonify(message='User register successfully'), 200
+#         return jsonify(message='User register successfully'), 200
 
-@api.route('/login', methods=['POST'])
-def login():
-    email = request.json.get("email", None)
-    password = request.json.get("password", None)
+# @api.route('/login', methods=['POST'])
+# def login():
+#     email = request.json.get("email", None)
+#     password = request.json.get("password", None)
 
-    user = User.query.fliter_by(email=email),first()
-    if user is None or not user.check_password(password):
-        return jsonify({"msg": "Incorrect email or password"}, 401)
+#     user = User.query.fliter_by(email=email),first()
+#     if user is None or not user.check_password(password):
+#         return jsonify({"msg": "Incorrect email or password"}, 401)
 
-        access_token = create_access_token(identify=email)
-        return jsonify(access_token=access_token)
+#         access_token = create_access_token(identify=email)
+#         return jsonify(access_token=access_token)
 
-@api.route('/logout', methods=['POST, GET'])
-def logout():
-    return jsonify(message='User logged out successfully'), 200
+# @api.route('/logout', methods=['POST, GET'])
+# def logout():
+#     return jsonify(message='User logged out successfully'), 200
 
-@api.route('/protected', methods=[GET])
-@jwt_required()
-def protected():
-    current_user_id = get_jwt_identify()
-    user = User.query.get(current_user_id)
+# @api.route('/protected', methods=[GET])
+# @jwt_required()
+# def protected():
+#     current_user_id = get_jwt_identify()
+#     user = User.query.get(current_user_id)
 
-    return jsonify({"id": user.id,"username": user.username}), 200
+#     return jsonify({"id": user.id,"username": user.username}), 200
