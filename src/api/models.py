4a831e9063711a14315e70c206422be002c29397
JohnCritch90 <123122544+JohnCritch90@users.noreply.github.com>
2023-06-15 22:49:26 +0000

    First Backend push

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,125 @@
+from sqlalchemy import TIMESTAMP, Column, ForeignKey, Integer, String, DateTime, Text, Date, create_engine
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship, declarative_base
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+  __tablename__ = 'user'
+  user_id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(255), unique=True, nullable=False)
+  email  = db.Column(String(255), unique=True, nullable=False)
+  password = db.Column(String(255), unique=True, nullable=False)
+
+  def __repr__(self):
+    return f'<User {self.name}>'
+  
+  def serialize(self):
+    return {
+      "user_id": self.user_id,
+      "username": self.username,
+      "email": self.email,
+      "password": self.password,
+    }
+  
+class JournalEntries (db.Model):
+  __tablename__ = 'journal_entries'
+  entry_id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer)
+  date = db.Column(db.Date, nullable=False)
+  mood = db.Column(db.String(255))
+  content = db.Column(db.Text)
+
+  def __repr__(self):
+    return f'<JournalEntries {self.name}>'
+
+  def serialize(self):
+    return {
+      "entry_id": self.entry_id,
+      "user_id": self.user_id,
+      "date": self.date,
+      "mood": self.mood,
+      "content": self.content,
+      "user": self.user
+    }
+  
+class MentalHealthResources (db.Model):
+  __tablename__ = 'mental_health_resources'
+  resource_id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(255), nullable=False)
+  description = db.Column(db.Text)
+  type = db.Column(db.String(255))
+  url = db.Column(db.String(255))
+
+  def __repr__(self):
+    return f'<MentalHealthResources {self.name}>'
+
+  def serialize(self):
+    return {
+      "resource_id": self.resource_id,
+      "title": self.title,
+      "description": self.description,
+      "type": self.type,
+      "url": self.url,
+    }
+  
+class MeditationSessions (db.Model):
+  __tablename__ = 'meditation_sessions'
+  session_id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(255), nullable=False)
+  duration = db.Column(db.Integer)
+  style = db.Column(db.String(255))
+  theme = db.Column(db.String(255))
+  youtube_url = db.Column(db.String(255))
+
+  def __repr__(self):
+    return f'<MeditationSessions {self.name}>'
+
+  def serialize(self):
+    return {
+      "session_id": self.session_id,
+      "title": self.title,
+      "duration": self.duration,
+      "style": self.style,
+      "theme": self.theme,
+      "youtube_url": self.youtube_url
+    }
+  
+class SupportGroups (db.Model):
+  __tablename__ = 'support_groups'
+  group_id  = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(255), nullable=False)
+  description = db.Column(db.Text)
+  meeting_link = db.Column(db.String(255))
+
+  def __repr__(self):
+    return f'<SupportGroups {self.name}>'
+
+  def serialize(self):
+    return {
+      "group_id": self.group_id,
+      "title": self.title,
+      "description": self.description,
+      "meeting_link": self.meeting_link,
+      "theme": self.theme
+    }
+  
+  class Notifications (db.Model):
+    __tablename__ = 'notifications'
+    notification_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    type = db.Column(db.String(255))
+    content = db.Column(db.Text)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+      return f'<Notifications {self.name}>'
 
     def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+      return {
+        "notification_id": self.notification_id,
+        "user_id": self.user_id,
+        "type": self.type,
+        "content": self.content,
+        "user": self.user
+      }
--- src/api/routes.py
+++ src/api/routes.py
@@ -1,18 +1,53 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
-from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
-from api.utils import generate_sitemap, APIException
+import os  
+from flask import Flask ,request, jsonify, Blueprint 
+from api.models import db, User 
+from flask_jwt_extended import create_access_token, get_jwt_identify, jew_required 
+from flask_jwt_extended import JWTManager  
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/signup', methods=['POST'])
+def signup():
+    email = request.json.get('email')
+    first_name = request.json.get('first_name')
+    last_name = request.json.get('last_name')
+    password = request.json.get('password')
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    if user.query.fliter_by(email=email).first():
+        return jsonify(message='Email already registered'), 200
 
-    return jsonify(response_body), 200
+        new-user = User(email, first_name=first_name ,last_name=last_name,password=password, is_active=Ture)
+
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            return jsonify(message='failed to register user'), 200
+
+        return jsonify(message='User register successfully'), 200
+
+@api.route('/login', methods=['POST'])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    user = User.query.fliter_by(email=email),first()
+    if user is None or not user.check_password(password):
+        return jsonify({"msg": "Incorrect email or password"}, 401)
+
+        access_token = create_access_token(identify=email)
+        return jsonify(access_token=access_token)
+
+@api.route('/logout', methods=['POST, GET'])
+def logout():
+    return jsonify(message='User logged out successfully'), 200
+
+@api.route('/protected', methods=[GET])
+@jwt_required()
+def protected():
+    current_user_id = get_jwt_identify()
+    user = User.query.get(current_user_id)
+
+    return jsonify({"id": user.id,"username": user.username}), 200
